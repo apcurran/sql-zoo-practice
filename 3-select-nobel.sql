f88aaeacb7da478873ce9554cc524aec0d3a52a4
Alex Curran <alexcurran.va@gmail.com>
2021-05-03 15:26:46 -0500

    Added up to 9

--- 3-select-nobel.sql
+++ 3-select-nobel.sql
@@ -22,3 +22,26 @@ WHERE subject = 'Peace' AND yr >= 2000;
 SELECT *
 FROM nobel
 WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
+
+-- 6.
+SELECT *
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+-- 7.
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%';
+
+-- 8.
+SELECT *
+FROM nobel
+WHERE
+    subject = 'Physics' AND yr = 1980
+    OR
+    subject = 'Chemistry' AND yr = 1984;
+
+-- 9.
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
