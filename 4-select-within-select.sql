70349800c717b7fe5896f46bba0ae9ea7f179f98
Alex Curran <alexcurran.va@gmail.com>
2021-05-04 13:33:43 -0500

    Finish query 4

--- 4-select-within-select.sql
+++ 4-select-within-select.sql
@@ -8,3 +8,45 @@ WHERE
         FROM world
         WHERE name = 'Russia'
     );
+
+-- 2.
+SELECT name
+FROM world
+WHERE
+    continent = 'Europe'
+    AND
+    gdp / population >
+    (
+        SELECT gdp / population
+        FROM world
+        WHERE name = 'United Kingdom'
+    );
+
+-- 3.
+SELECT name, continent
+FROM world
+WHERE continent IN
+                (
+                    SELECT continent
+                    FROM world
+                    WHERE name IN ('Argentina', 'Australia')
+                )
+ORDER BY name;
+
+-- 4.
+SELECT name
+FROM world
+WHERE
+    population >
+    (
+        SELECT population
+        FROM world
+        WHERE name = 'Canada'
+    )
+    AND
+    population <
+    (
+        SELECT population
+        FROM world
+        WHERE name = 'Poland'
+    );
