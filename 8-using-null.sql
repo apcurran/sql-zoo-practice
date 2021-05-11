96a6f905a2a8314d3c8f64aaccd68c9c794d732a
Alex Curran <alexcurran.va@gmail.com>
2021-05-11 11:53:11 -0500

    Finish query 3

--- 8-using-null.sql
+++ 8-using-null.sql
@@ -2,3 +2,17 @@
 SELECT name
 FROM teacher
 WHERE dept IS NULL;
+
+-- 2.
+SELECT
+    teacher.name,
+    dept.name
+FROM teacher
+INNER JOIN dept ON teacher.dept = dept.id;
+
+-- 3.
+SELECT
+    teacher.name,
+    dept.name
+FROM teacher
+LEFT JOIN dept ON teacher.dept = dept.id;
