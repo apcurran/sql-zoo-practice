6e8edeef3376768a51432418f898bdefcbbd03ca
Alex Curran <alexcurran.va@gmail.com>
2021-05-11 11:48:33 -0500

    Add section 8 and query 1

--- 7-more-join-operations.sql
+++ 7-more-join-operations.sql
@@ -34,3 +34,31 @@ SELECT name
 FROM actor
 INNER JOIN casting ON actor.id = casting.actorid
 WHERE movieid = (SELECT id FROM movie WHERE title = 'Casablanca');
+
+-- 7.
+SELECT name
+FROM actor
+INNER JOIN casting ON actor.id = casting.actorid
+WHERE movieid = (SELECT id FROM movie WHERE title = 'Alien');
+
+-- 8.
+SELECT movie.title
+FROM movie
+INNER JOIN casting ON movie.id = casting.movieid
+WHERE casting.actorid = (SELECT id from actor WHERE name = 'Harrison Ford');
+
+-- 9.
+-- WHERE casting.ord != 1 -- Movies where Harrison Ford appeared, but not in a leading role.
+SELECT movie.title
+FROM movie
+INNER JOIN casting ON movie.id = casting.movieid
+WHERE casting.actorid = (SELECT id from actor WHERE name = 'Harrison Ford') AND casting.ord != 1;
+
+-- 10.
+SELECT
+    movie.title,
+    actor.name
+FROM movie
+INNER JOIN casting ON movie.id = casting.movieid
+INNER JOIN actor ON casting.actorid = actor.id
+WHERE movie.yr = 1962 AND casting.ord = 1;
---
+++ 8-using-null.sql
@@ -0,0 +1,4 @@
+-- 1.
+SELECT name
+FROM teacher
+WHERE dept IS NULL;
