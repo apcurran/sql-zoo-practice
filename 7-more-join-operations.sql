6e55b4227418ab97f06eb47b4e7a315deff0fff7
Alex Curran <alexcurran.va@gmail.com>
2021-05-11 11:17:58 -0500

    Finish up to query 6

--- 7-more-join-operations.sql
+++ 7-more-join-operations.sql
@@ -18,3 +18,19 @@ SELECT
 FROM movie
 WHERE title LIKE '%Star Trek%'
 ORDER BY yr;
+
+-- 4.
+SELECT actor.id
+FROM actor
+WHERE name = 'Glenn Close';
+
+-- 5.
+SELECT movie.id
+FROM movie
+WHERE title = 'Casablanca';
+
+-- 6.
+SELECT name
+FROM actor
+INNER JOIN casting ON actor.id = casting.actorid
+WHERE movieid = (SELECT id FROM movie WHERE title = 'Casablanca');
