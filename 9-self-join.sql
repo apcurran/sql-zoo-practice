e0a50b3bb84710a9f68ec2f5cc7cac3588d057f2
Alex Curran <alexcurran.va@gmail.com>
2021-05-14 12:54:14 -0500

    Query 6

--- 9-self-join.sql
+++ 9-self-join.sql
@@ -35,3 +35,14 @@ FROM route a
 INNER JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop = 53 AND b.stop = 149;
+
+-- 6.
+SELECT
+    a.company, a.num, stopa.name,
+    stopb.name
+FROM route a
+INNER JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road';
